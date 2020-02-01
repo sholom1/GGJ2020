dbce7a028365310082f27bff600473636c7f9c59
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-02-01 10:48:50 -0500

    Added trigger base class

---
+++ New Unity Project/Assets/Scripts/LeakManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeakManager : MonoBehaviour
+{
+    public List<Trigger> Pipes;
+    public UnityEvent OnRepaired;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreach (Trigger pipe in Pipes)
+        //{
+        //    if (pipe.RepairJobs[0].TimeToRepair > 0f)
+        //    {
+        //        if (Input.GetKey(pipe.RepairJobs[0].RequiredKey))
+        //        {
+        //            pipe.RepairJobs[0].TimeToRepair -= Time.deltaTime;
+        //        }
+        //        else
+        //        {
+        //            pipe.RepairJobs[0].TimeToExplode -= Time.deltaTime;
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Trigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    public UnityEvent OnPressed;
+    public UnityEvent OnReleased;
+    public KeyCode RequiredKey;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(RequiredKey))
+        {
+            OnPressed.Invoke();
+        }
+        else if (Input.GetKeyUp(RequiredKey))
+        {
+            OnReleased.Invoke();
+        }
+    }
+    public void TestPressed()
+    {
+        Debug.Log("The key has been pressed");
+    }
+    public void TestRelease()
+    {
+        Debug.Log("The key has been released");
+    }
+}
+public class Leak
+{
+    public KeyCode RequiredKey;
+    public float TimeToExplode;
+    public float TimeToRepair;
+    public float TimeBetweenLeaks;
+}
