e1cd19359b5e51ef1ed7f4d7c095393d9647d96f
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-02-01 16:47:49 -0500

    Updated trigger size

---
+++ hammerrepair/Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+    public List<Trigger> Triggers;
+    public int nextLevel;
+    void Update()
+    {
+        foreach (Trigger trigger in Triggers)
+        {
+            if (!Input.GetKey(trigger.RequiredKey))
+            {
+                return;
+            }
+        }
+        GameController.Instance.LevelWin();
+    }
+}
