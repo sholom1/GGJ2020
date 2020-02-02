cfcf55ad184981dbf01615cf9a6bd17e33ed529a
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-02-01 20:14:38 -0500

    Fixed input inaccuracy maybe?

--- hammerrepair/Assets/Scripts/Trigger.cs
+++ hammerrepair/Assets/Scripts/Trigger.cs
@@ -25,18 +25,11 @@ public class Trigger : MonoBehaviour
             OnReleased.Invoke();
         }   
     }
-    private void Start()
-    {
-        RequiredKey = InputDictionary.instance.KeyPool[Random.Range(0, InputDictionary.instance.KeyPool.Count)];
-        InputDictionary.instance.KeyPool.Remove(RequiredKey);
-        Letter.text = RequiredKey.ToString();
-    }
     private void Awake()
     {
         Debug.Log("Enabled");
         OnEnabled.Invoke();   
     }
-
     public void OnDrawGizmos()
     {
         Gizmos.DrawSphere(transform.position, 1000);
--- hammerrepair/Assets/Scripts/TriggerManager.cs
+++ hammerrepair/Assets/Scripts/TriggerManager.cs
@@ -6,6 +6,15 @@ public class TriggerManager : MonoBehaviour
 {
     public List<Trigger> Triggers;
     public int nextLevel;
+    private void Start()
+    {
+        foreach (Trigger trigger in Triggers)
+        {
+            trigger.RequiredKey = InputDictionary.instance.KeyPool[Random.Range(0, InputDictionary.instance.KeyPool.Count)];
+            InputDictionary.instance.KeyPool.Remove(trigger.RequiredKey);
+            trigger.Letter.text = trigger.RequiredKey.ToString();
+        }
+    }
     void Update()
     {
         foreach (Trigger trigger in Triggers)
