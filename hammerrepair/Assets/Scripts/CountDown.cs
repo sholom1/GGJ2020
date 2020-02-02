4f5d872ef11da906e0b7fee5859693e11a0c0da7
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-02-01 21:57:26 -0500

    Working on countdown UI and sounds

--- hammerrepair/Assets/Scripts/CountDown.cs
+++ hammerrepair/Assets/Scripts/CountDown.cs
@@ -3,33 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CountDown : MonoBehaviour
 {
     public AudioSource source;
-    public AudioClip[] AudioClips;
+    public AudioClip Clip;
+    public CountdownNumberClipPair[] AudioNumberPairs;
     public float TimeBetweenClips;
-    public TextMeshProUGUI TimerText;
+    public Image TimerGUI;
+    public Image CircleUI;
+    public Image StartUI;
 
     public UnityEvent OnTimerComplete;
     private void Start()
     {
         StartCoroutine(CountDownCoroutine());
     }
+
+    [System.Obsolete]
     private IEnumerator CountDownCoroutine()
     {
-        for(int i = AudioClips.Length-1; i >= 0; i--)
+        source.clip = Clip;
+        source.Play();
+        for(int i = 0; i < AudioNumberPairs.Length; i++)
         {
-            float timer = TimeBetweenClips + AudioClips[i].length;
-            source.clip = AudioClips[i];
-            source.Play();
-            TimerText.text = $"{1 + i}";
-            yield return new WaitForSeconds(timer);
+            float timer = AudioNumberPairs[i].TimeStamp;
+            TimerGUI.sprite = AudioNumberPairs[i].Number;
+            Debug.Log(timer);
+            while (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                CircleUI.transform.rotation.SetEulerRotation(new Vector3(0, 0, Mathf.Rad2Deg * 360f * (timer / AudioNumberPairs[i].TimeStamp)));
+                yield return null;
+            }
         }
-        TimerText.text = "GO!";
+        TimerGUI.gameObject.SetActive(false);
+        CircleUI.gameObject.SetActive(false);
+        StartUI.gameObject.SetActive(true);
         yield return new WaitForSeconds(TimeBetweenClips);
         OnTimerComplete.Invoke();
 
         //GameController.Instance.LevelStart();
     }
+}
+[System.Serializable]
+public class CountdownNumberClipPair
+{
+    public float TimeStamp;
+    public Sprite Number;
 }
