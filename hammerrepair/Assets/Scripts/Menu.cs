a671408d1662a2504bd3361390db406380c57101
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-02-02 13:52:43 -0500

    Audio Settings added

--- hammerrepair/Assets/Scripts/Menu.cs
+++ hammerrepair/Assets/Scripts/Menu.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Slider AudioSlider;
     public void PlayGame()
     {
         GameController.Instance.GameStart();
     }
-    public void UpdateSoundUI()
+    public void AdjustMasterVolume()
     {
-
+        AudioListener.volume = AudioSlider.value / AudioSlider.maxValue;
+    }
+    public void MuteMasterAudio(bool toggle)
+    {
+        AudioListener.volume = toggle ? 0 : AudioSlider.value / AudioSlider.maxValue;
     }
     public void QuitGame()
     {
